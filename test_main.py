a4f8d8f5022f9d67e772a97eaa97c7d396b1c9c3
kevin lee <klee041185@gmail.com>
2024-01-30 14:24:53 +0000

    shop commit

--- test_main.py
+++ test_main.py
@@ -8,13 +8,8 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 def verify_title():
     driver.get("https://sdetunicorns.com")
     title = driver.title
-    print(title)
-    expected_title = "Master Software Tsting and Automation | SDET Unicorns"
-    if title == expected_title:
-        print("get in")
-    else:
-        print("oh no")
-    driver.quit()
+    expected_title = "Master Software Testing and Automation | SDET Unicorns"
+    assert title == expected_title
 
 
 if __name__ == '__main__':
@@ -23,3 +18,4 @@ if __name__ == '__main__':
 
 def test_001():
     verify_title()
+
