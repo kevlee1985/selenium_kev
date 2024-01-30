9d7c98e76cd51545b8bb758f6224e58907843573
kevin lee <klee041185@gmail.com>
2024-01-30 13:59:11 +0000

    shop commit

---
+++ test_main.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
+def verify_title():
+    driver.get("https://sdetunicorns.com")
+    title = driver.title
+    print(title)
+    expected_title = "Master Software Testing and Automation | SDET Unicorns"
+    if title == expected_title:
+        print("get in")
+    else:
+        print("oh no")
+    driver.quit()
+
+
+if __name__ == '__main__':
+    verify_title()
+
+
+def test_001():
+    verify_title()
